6758c1fea49384778023815dac6a321222b9def3
livia01px2019 <livia.zhu@live.com>
2022-02-05 12:24:20 -0500

    stencil bug fix

--- sec1/swap.c
+++ sec1/swap.c
@@ -5,11 +5,12 @@ void weird_swap(int **arr, int *num){
 	// TODO: Swap the first element of arr with num! 
 }
 int main(){
-	int* arr = {2, 2};
+	int arr[2] = {2, 2};
+	int* arr_ptr = arr;
 	int num = 1;
-	
-	weird_swap(&arr, &num);
-	printf("Weird swap results: %d, %d\n", *arr, num);
+
+	weird_swap(&arr_ptr, &num);
+	printf("Weird swap results: %d, %d\n", arr[0], num);
 
 	return 0;
 }
